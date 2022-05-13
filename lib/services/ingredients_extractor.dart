4f30f5fb30e2caab6b66de00ec88f2b1a96a57c9
Ebinu-s <ebinusuneer2nd@gmail.com>
2022-05-13 23:46:38 +0530

    added ingridient extractor

--- lib/services/ingredients_extractor.dart
+++ lib/services/ingredients_extractor.dart
@@ -1,2 +1,35 @@
 // extract and return ingredients from input array of strings
-import 'dart:io';
+import 'dart:developer'; // delete on production
+
+class IngredientExtractor {
+
+  List<String> extractTheIngredients(List blocks) {
+
+    List<String> result = [];
+    bool nextHasIngredients = false; //sets true if the ingredients are on next block
+
+    for (String element in blocks) {
+
+      if(nextHasIngredients) {
+        List<String> temp = element.split(',');
+        result = temp;
+        break;
+      }
+
+      // check if the block contains word INGREDIENT[s]
+      if(element.contains("INGREDIENTS") || element.contains("INGREDIENT")) {
+        // validate if ingredients are in this block or next
+        if(element.length > 12 ){
+          List<String> temp = element.substring(13).split(',');
+          result = temp;
+          break;
+        }
+        else {
+          nextHasIngredients = true;
+        }
+      }
+    }
+
+    return result;
+  }
+}
